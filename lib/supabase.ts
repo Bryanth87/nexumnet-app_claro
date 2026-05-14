cadd4ac27b95dcd6d88b462f1a7db762e56b0132
itd-dev-cloud <itd.dev@intertd.com>
2026-05-14 16:57:51 -0600

    fix: add comentary in lib/supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -4,7 +4,7 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!
 
-// Cliente público (anon key) — solo lectura de precios
+// Cliente público (anon key) — solo lectura de preciossss
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
 // Cliente admin (service role) — usado solo en API routes del servidor
